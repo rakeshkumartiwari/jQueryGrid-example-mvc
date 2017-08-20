e3a16eca6f0d55eb5aea31cd98f577bc2dd6a707
rakeshkumartiwari <tiwarirakesh8@gmail.com>
2017-08-20 23:38:46 +0530

    jqGrid Example mvc

---
+++ JqGridExampleMVC/Controllers/HolidayController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JqGridExampleMVC.Controllers
+{
+    public class HolidayController : Controller
+    {
+        // GET: Holiday
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetHolidays()
+        {
+            using (var dc = new HolidayDbEntities())
+            {
+                var holidays = dc.Holidays.ToList();
+                return Json(holidays, JsonRequestBehavior.AllowGet );
+            }
+
+        }
+    }
+   
+}
